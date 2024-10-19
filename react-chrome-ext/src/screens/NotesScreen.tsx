0c86800feb10f83489dec02c81b516fc30498392
Acidixzz <chasemccainn@aol.com>
2024-10-19 15:04:00 -0700

    changing notes functionality

--- react-chrome-ext/src/screens/NotesScreen.tsx
+++ react-chrome-ext/src/screens/NotesScreen.tsx
@@ -1,15 +1,59 @@
+/// <reference types="chrome"/>
+
+import { useEffect, useState } from "react";
+
 type NotesProps = {
     backToHome: () => void;
 }
 
 export default function Notes(props: NotesProps) {
 
+    const [userNotes, setUserNotes] = useState<string[]>([]);
+    const [note, setNote] = useState('');
+
+    useEffect(() => {
+        loadSavedNotes();
+    }, [])
+
+    // load saved notes (on startup and after saving a new note)
+    function loadSavedNotes() {
+        chrome.storage.sync.get('userNotes', (data) => {
+            setUserNotes(data.userNotes);
+        })
+    }
+
+    function saveNotes() {
+        let notes = userNotes;
+        if (notes.length >= 50) {
+            alert('Maximum of 50 notes reached.');
+            return;
+        }
+        notes.push(note);
+        chrome.storage.sync.set({ 'userNotes': notes }, () => {
+            console.log('Note saved');
+        });
+
+        loadSavedNotes();
+    }
+
+    // delete note
+    function deleteNote(index: number) {
+        chrome.storage.sync.get('userNotes', (data) => {
+            let notes = data.userNotes || [];
+            notes.splice(index, 1);
+            chrome.storage.sync.set({ 'userNotes': notes }, () => {
+                console.log('Note deleted');
+                loadSavedNotes();
+            });
+        });
+    }
+
     return (
         <div id="notes-page">
             <h2>Notes</h2>
-            <textarea id="note-textarea" placeholder="Type your notes here..." rows={4} cols={30}></textarea>
+            <textarea id="note-textarea" placeholder="Type your notes here..." rows={4} cols={30} onChange={(event) => setNote(event.target.value)} value={note}></textarea>
             <br />
-            <button id="save-note">Save Note</button>
+            <button id="save-note" onClick={saveNotes}>Save Note</button>
             <div id="saved-notes"></div>
             <button id="back-to-home" onClick={props.backToHome}>Back to Home</button>
         </div>
