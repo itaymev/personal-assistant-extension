c13c06d44d4dc0c291ccb17f33c2b5d30f9748c1
ereinha3 <ethanreinhart@gmail.com>
2024-10-19 19:18:44 -0700

    changes

--- new-backend/main.py
+++ new-backend/main.py
@@ -15,6 +15,7 @@ summarizer = pipeline("summarization", device=device)
 def extract_article():
     print("Received a request to summarize.")
     url = request.json.get('url')
+    length = request.json.get('length', 0)
     print(f"URL received: {url}")
     
     if not url:
@@ -23,15 +24,34 @@ def extract_article():
     # Extract article content
     try:
         article = goose.extract(url=url)
+
+        print(f"Title: {article.title}")
+        print(f"Meta Description: {article.meta_description}")
+        print(f"Meta Keywords: {article.meta_keywords}")
+        print(f"Publish Date: {article.publish_date}")
+        print(f"Additional Date: {article.additional_data}")
+        print(f"Doc: {article.doc}")
+        print(f"Authors: {article.authors}")
+
+
+
         content = article.cleaned_text
         print(f"Extracted content: {content}")
+
+        if article.meta_description and length == 0:
+            return jsonify({'text': content, 'summary': article.meta_description})
+    
+        else:
+            maxLength = 150 + 200 * length
+            print(maxLength)
+            summary = summarizer(content, max_length=150, min_length=30, do_sample=True)
+            return jsonify({'text': content, 'summary': summary[0]['summary_text']})
+
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-    # Summarize the content
-    summary = summarizer(content, max_length=150, min_length=30, do_sample=False)
-    return jsonify({'text': content, 'summary': summary[0]['summary_text']})
-
+    
+   
 
 if __name__ == '__main__':
     app.run(port=5000)
